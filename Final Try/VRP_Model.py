5fbe88d748b5e919a9781541a9a7f6da83d2016d
Dimitris Matsanganis <jimmatsa@hotmail.gr>
2023-01-14 19:47:58 +0200

    Add files via upload

--- Final Try/VRP_Model.py
+++ Final Try/VRP_Model.py
@@ -4,54 +4,45 @@ import math
 
 class Model:
 
-    # instance variables
     def __init__(self):
         self.allNodes = []
         self.customers = []
         self.matrix = []
-        self.distance_matrix = []
         self.capacity = -1
 
     def BuildModel(self):
         random.seed(1)
-        depot = Node(0, 50, 50, 0, 0)
+
+        # Initialize depot at the middle of the map (x=y=50).
+        depot = Node(0, 50, 50, 0)
         self.allNodes.append(depot)
+        self.capacity = 50
 
-        self.capacity = 3000
-        cust_num = 200
-
-        for i in range(cust_num):
+        # Initialize total customers and through a for loop statement create
+        # randomly (random seed 1) the 200 customers.
+        totalCustomers = 200
+        for i in range(0, totalCustomers):
             x = random.randint(0, 100)
             y = random.randint(0, 100)
             dem = 100 * (1 + random.randint(1, 4))
-            unloading_time = 0.25 #1/4 of hour.
-            customers = Node(i + 1, x, y, dem, unloading_time)
-            self.allNodes.append(customers)
-            self.customers.append(customers)
+            cust = Node(i + 1, x, y, dem)
+            self.allNodes.append(cust)
+            self.customers.append(cust)
 
         rows = len(self.allNodes)
         self.matrix = [[0.0 for x in range(rows)] for y in range(rows)]
-        self.distmatrix = [[0.0 for x in range(rows)] for y in range(rows)]
 
+        # Calculate distance for all nodes through an inner forloop statement.
         for i in range(0, len(self.allNodes)):
-
             for j in range(0, len(self.allNodes)):
                 a = self.allNodes[i]
                 b = self.allNodes[j]
                 dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
-                time = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2)) / 40 + unloading_time
-
-                if j != 0:
-                    self.distmatrix[i][j] = dist
-                    self.matrix[i][j] = time
-                else:
-                    self.distmatrix[i][j] = 0
-                    self.matrix[i][j] = 0
+                self.matrix[i][j] = dist
 
 
 class Node:
-    def __init__(self, idd, xx, yy, dem, unloading_time):
-        self.unloading_time = unloading_time
+    def __init__(self, idd, xx, yy, dem):
         self.x = xx
         self.y = yy
         self.ID = idd
